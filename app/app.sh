0162b4b1ae21b5d47f08605e7310ad0b561b7c54
eduardo.silva <j98541@amcontratos.com.br>
2024-07-28 11:37:34 -0300

    Fila e Workers

--- app/app.sh
+++ app/app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # pyscopg2 - acessar banco de dados postgres
-pip install bottle==0.12.13 psycopg2==2.7.3.2
+pip install bottle==0.12.13 psycopg2==2.7.3.2 redis==2.10.5
 python -u sender.py
